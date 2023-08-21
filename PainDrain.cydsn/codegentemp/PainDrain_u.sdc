090e7994779eec43c9d32d6022d751a8ab1c30b0
Bailey Haskell <Bailey Haskell>
2023-08-21 09:45:54 -0600

    first commit

---
+++ PainDrain.cydsn/codegentemp/PainDrain_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Projects\Pain Drain\Production2\Firmware\PainDrain\PainDrain.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Projects\Pain Drain\Production2\Firmware\PainDrain\PainDrain.cydsn\PainDrain.cyprj
+# Date: Fri, 18 Aug 2023 22:43:05 GMT
---
+++ PainDrain.cydsn/myUART.c
@@ -0,0 +1,26 @@
+/* ========================================
+ *
+ * Copyright FinkyDoodle, 2020
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF FinkyDoodle
+ *
+ * ========================================
+*/
+
+#include "myUART.h"
+
+//Requires a GPIO PIN named MY_UART_PIN that is STRONG drive set high
+
+void myUART_PutChar(char c) {
+    
+    int shift_reg = (int)c;
+    
+    
+    
+}
+
+
+/* [] END OF FILE */
